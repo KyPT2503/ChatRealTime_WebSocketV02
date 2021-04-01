c2317a1f53feeca7562fb2d6cb2f71849cc619f1
Phan Thế Kỷ <thekypccc@gmail.com>
2021-04-01 22:57:18 +0700

    create project

---
+++ src/main/java/demo/chat_realtime_v02/ChatRealtimeV02Application.java
@@ -0,0 +1,13 @@
+package demo.chat_realtime_v02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRealtimeV02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatRealtimeV02Application.class, args);
+    }
+
+}
---
+++ src/test/java/demo/chat_realtime_v02/ChatRealtimeV02ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.chat_realtime_v02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRealtimeV02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
